4ecdc7bfc646688787d47ce23afa8fe511dda769
Heinz N. Gies <heinz@licenser.net>
2016-12-19 22:13:42 +0100

    Build update

---
+++ pkg/pkg.mk
@@ -0,0 +1,9 @@
+include ../../leofs.mk
+VERSION=$(LEOFS_VERSION)
+PKG_CATEGORY=leofs
+PKG_HOMEPAGE=http://www.leofs.org/
+DEPS="erlang>=18.0.0"
+TARGET_DIR=/opt/local
+COMPONENT_PATH=../../package/$(COMPONENT_INTERNAL)
+DIR=$(STAGE_DIR)/$(COMPONENT)
+include ../../deps/fifo_utils/priv/pkg.mk
