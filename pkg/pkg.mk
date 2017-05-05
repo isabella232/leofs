da337d1e329099009b8fd91907a85b932d5c6c01
Heinz N. Gies <heinz@licenser.net>
2017-05-05 23:05:29 +0200

    Fix deps

--- pkg/pkg.mk
+++ pkg/pkg.mk
@@ -1,4 +1,4 @@
-include ../leofs.mk
+include ../../leofs.mk
 VERSION=$(LEOFS_VERSION)
 PKG_CATEGORY=leofs
 PKG_HOMEPAGE=https://leo-project.net/leofs/
@@ -6,4 +6,4 @@ DEPS="erlang>=18.0.0"
 TARGET_DIR=/opt/local
 COMPONENT_PATH=../../package/$(COMPONENT_INTERNAL)
 DIR=$(STAGE_DIR)/$(COMPONENT)
-include ../deps/fifo_utils/priv/pkg.mk
+include ../../deps/fifo_utils/priv/pkg.mk
